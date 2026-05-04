14a0c335a4ca47bbf16acf541d5684e5a72807ed
Abdessamad-PRO <abdessamadaitelmahjoub2011@gmail.com>
2026-05-04 20:12:55 +0100

    Modification de products.ts pour utiliser l'implémentation avec a methode onInit

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -7,12 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.css',
   standalone: true
 })
-export class ProductsComponent {
-  products = [
+export class ProductsComponent implements OnInit {
+  products! :any ;
+  constructor() { }
+
+ ngOnInit(): void {
+     this.products=[
     { id: 1, name: 'Laptop', price: 1200, selected:true },
     { id: 2, name: 'Mouse', price: 25, selected:false},
     { id: 3, name: 'Keyboard', price: 45, selected:true}
   ];
-
+ }
 
 }
